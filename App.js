7195b798b9a011dd9ee9dd5c43bd043e0bcd5c9e
Carlos Cortés <tky2048@gmail.com>
2020-02-26 21:50:10 -0600

    Final commit

--- App.js
+++ App.js
@@ -6,8 +6,7 @@
  * @flow
  */
 import React from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {StatusBar} from 'react-native';
 import Home from './components/Home';
 import LyricsScreen from './components/LyricsScreen';
 import {NavigationContainer} from '@react-navigation/native';
@@ -27,44 +26,4 @@ const App: () => React$Node = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
 export default App;
